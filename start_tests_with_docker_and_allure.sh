cb2336bd7fc86c08cb092559be43af82c3f99d95
dzailz <katin.v.v@gmail.com>
2019-12-03 15:32:23 +0300

    added catalog with allure results. Some changes in start_test_with_docker_and_allure.sh

--- start_tests_with_docker_and_allure.sh
+++ start_tests_with_docker_and_allure.sh
@@ -3,6 +3,6 @@
 source ~/stepik_traineeship/env/bin/activate
 cd ~/stepik_traineeship/edy-tests/Webdriver/scripts/
 echo "Starting scripts"
-gnome-terminal -x sh -c "py.test --alluredir=~/stepik_traineeship/allure/ -v -s -m smoke --executor=http://127.0.0.1:4444/wd/hub --domain=https://staging1.int.stepik.org -n2; bash"
+gnome-terminal -x sh -c "py.test --alluredir=/home/dzailz/stepik_traineeship/allure/ -v -s -m smoke --executor=http://127.0.0.1:4444/wd/hub --domain=https://staging1.int.stepik.org -n2 --browser=chrome ; bash"
 sleep 2
-py.test --alluredir=~/stepik_traineeship/allure/ -v -s -m smoke --executor=http://127.0.0.1:4444/wd/hub --domain=https://staging1.int.stepik.org --browser=chrome -n2
+py.test --alluredir=/home/dzailz/stepik_traineeship/allure/ -v -s -m smoke --executor=http://127.0.0.1:4444/wd/hub --domain=https://staging1.int.stepik.org --browser=firefox -n2
